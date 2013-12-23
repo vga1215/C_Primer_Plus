e7402d23701f00f8401ef7cb8609f3a9566c0cc9
shawn <vga1215@gmail.com>
2013-12-23 16:56:17 +0800

    update ch01

---
+++ c5ex/ch01/inform.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main (void)
+{
+
+    printf ("A.c is used to end a c program filename.\n");
+    return 0;
+}
---
+++ c5ex/ch02/first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main (void)       /*一个简单的Ｃ程序例子*/
+{
+
+    int num;          /*定义一个num的变量*/
+    num = 1;         /*为变量num赋值    */
+
+    printf ("i am a simple "); /* 使用库函数printf() */
+    printf ("computer.\n");
+    printf ("my favorite number is %d because it is first.\n", num);
+    return 0;
+}
